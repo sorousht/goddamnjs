5f8640b5d0550bd356becc0ed1781fcf516ccf57
Mahdis Sabzevarzade <mahdis.sabzevarzadeh@check24.de>
2020-04-20 18:24:30 +0200

    solution

--- src/e02-lowest-value/index.js
+++ src/e02-lowest-value/index.js
@@ -4,9 +4,16 @@
     It has been said that one of the most basic exercises of Japanese warriors was playing with arrays!
     Array is like fire. You need to master it, otherwise it burns you! Yohahaha 
 */
+function compareNumbers(a, b) {
+    return a - b;
+}
+
+// 78 - 56 = 22     =>   56
+// 56 - 232 = -176  =>   56
+// 232 - 12 = 220   =>   12
 
 const getLowestItem = (items) => {
-    // TODO: Implement 
+    return items.sort(compareNumbers)[0]
 };
 
 module.exports = getLowestItem;
