0c04e131f81e32c9cd77879616ec313715bbdf2a
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2020-04-20 12:15:55 +0200

    Ad comment to the exercise

--- src/e02-lowest-value/index.js
+++ src/e02-lowest-value/index.js
@@ -1,3 +1,10 @@
+/*
+    Hey JS warrior, welcome to the second task!
+    
+    It has been said that one of the most basic exercises of Japanese warriors was playing with arrays!
+    Array is like fire. You need to master it, otherwise it burns you! Yohahaha 
+*/
+
 const getLowestItem = (items) => {
     // TODO: Implement 
 };
