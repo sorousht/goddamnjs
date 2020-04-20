9c7be98182bb2d42a13e5b5a69e7c7c0d92a5aa7
Mahdis Sabzevarzade <mahdis.sabzevarzadeh@check24.de>
2020-04-20 17:10:12 +0200

    improved version

--- src/e01-abbr/index.js
+++ src/e01-abbr/index.js
@@ -4,60 +4,12 @@
     - Will Smith -> W.S
     - Natalie Portman -> N.P
 */
+
+const getFirstLetter = (element) => {
+    return element.slice(0,1)
+}
 const getInitials = (fullName) => {
-    let result = [];
-    let initials
-    let final
-    if (fullName === '') {
-        return ''
-    } else {
-        const arrayOfStrings = fullName.split(' ')
-        if (arrayOfStrings.length === 1) {
-            final = arrayOfStrings[0].slice(0, 1) + '.'
-        } else {
-            arrayOfStrings.forEach(element => {
-                initials = element.slice(0, 1)
-                result.push(initials)
-            })
-            final = result.join('.')
-
-        }
-        console.log(final)
-        return final
-
-    }
-
-
-// if(fullName === '') {
-//     return '';
-// } else {
-
-//     let firstNameInitial = fullName.slice(0,1);
-//     let lastNameInitial = '';
-
-//     for (let i=0 ;i <= fullName.length; i++) {
-        
-//         let char = fullName[i];
-//         if (char === ' ') {
-            
-//            lastNameInitial = fullName.slice(i+1,i+2)
-//         }
-//     }
-//     console.log(firstNameInitial + '.' + lastNameInitial)
-//     return firstNameInitial + '.' + lastNameInitial;
-// }
-
-
+    return fullName.split(' ').map(getFirstLetter).join('.');
 };
 
-// getInitials("Jim Carrey derr");
-
 module.exports = getInitials;
-
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
-// node src/e01-abbr/index.js
-
-/*
-    git remote -v
-    git status
- */
